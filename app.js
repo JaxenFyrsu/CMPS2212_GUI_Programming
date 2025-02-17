c78a7757a22f28648986a68537195a0cd46b1f99
JaxenFyrsu <jdone4man@gmail.com>
2025-02-17 11:18:54 -0700

    refactor routes

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
-import express from "express";
+import express from 'express';
+import signupRoutes from './routes/signupRoutes.js';
 
 const app = express();
 
-//middleware
-app.use((req, res, next) => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.url}`);
-  next();
-});
+// Middleware to parse form data and make it available to us in req.body
+app.use(express.urlencoded({ extended: true }));
 
-const adminMiddleware = (req, res, next) => {
-  if (req.headers["x-admin"] === "true") {
-    console.log("[Access granted to /pay");
-    next();
-  } else {
-    console.log("[Access granted to /pay");
-    res.status(403).send("403 Forbidden: Admin access required\n");
-  }
+const loggingMiddleware = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
 };
 
-// Route for home page
-// Headers automatically set and written
-app.get("/", (req, res) => {
-  res.send("Home page\n");
-});
+app.use(loggingMiddleware);
+// Use the signup routes
+app.use('/', signupRoutes);
 
-// Route for pay page
-// Headers automatically set and written
-app.get("/pay", adminMiddleware, (req, res) => {
-  res.send("Pay page\n");
-});
-
-// Route for about page
-// Headers automatically set and written
-app.get("/about", (req, res) => {
-  res.send("What do you want to know?\n");
-});
-
-// Catch-all route for 404 errors. Our first taste of middleware
 app.use((req, res) => {
-  res.status(404).send("404 Not Found.\n");
+  res.status(404).send('Oops! The page you are looking for does not exist.');
 });
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
